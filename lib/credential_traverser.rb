6c86f07cd8257887aae37c0f05c6267d0b60636b
Masao Mochizuki <mochizuki.masao@classmethod.jp>
2014-09-22 14:11:51 +0900

    passing profile name for the block

--- lib/credential_traverser.rb
+++ lib/credential_traverser.rb
@@ -1,12 +1,13 @@
 require "credential_traverser/version"
+require "aws-sdk-v1"
 
 module CredentialTraverser
   def self.traverse
     raise ArgumentError('CredentialTraverser#traverse must be used with block') unless block_given?
     profiles = AWS::Core::IniParser.parse(File.read(File.join(Dir.home, '.aws', 'credentials')))
     profiles.keys.map do |profile|
-      AWS.config(credential_provider: AWS::Core::Providers::CredentialFileProvider.new(profile_name: profile))
-      yield
+      AWS.config(credential_provider: AWS::Core::CredentialProviders::SharedCredentialFileProvider.new(profile_name: profile))
+      yield profile
     end
   end
 end
